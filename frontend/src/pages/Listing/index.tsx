33e8962f74d0ed6fff591d60604d723b0ae700df
ianpessoa <ianzanon2@gmail.com>
2022-03-13 16:53:39 -0300

    Pagination Layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,19 +1,8 @@
-import {ReactComponent as Arrow} from '../../assets/img/arrow.svg';
-import './styles.css';
+import Pagination from "components/Pagination";
 
 function Listing() {
     return (
-        <div className="dsmovie-pagination-container">
-        <div className="dsmovie-pagination-box">
-            <button className="dsmovie-pagination-button" disabled={true} >
-                <Arrow />
-            </button>
-            <p>{`${1} de ${3}`}</p>
-            <button className="dsmovie-pagination-button" disabled={false} >
-                <Arrow className="dsmovie-flip-horizontal" />
-            </button>
-        </div>
-    </div>
+        <Pagination/>
     )
 }
 
